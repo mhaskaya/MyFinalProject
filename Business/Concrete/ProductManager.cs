e3b13d4587feff11b4cda17b07213e0bcffce566
mhaskaya <mhaskaya12@gmail.com>
2021-01-31 18:03:07 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _produtDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _produtDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _produtDal.GetAll();
+        }
+    }
+}
